d45bec644e93f3bf1147cf08099b6e7abb4ec27b
Paul Gaston <paul.gaston@sfr.fr>
2023-04-26 02:27:43 +0200

    [ADD] Add visual indications for the ultimate

--- Assets/Scripts/UltimateLoad.cs
+++ Assets/Scripts/UltimateLoad.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UltimateLoad : MonoBehaviour {
     private float YinFill = 0f;
@@ -9,6 +10,12 @@ public class UltimateLoad : MonoBehaviour {
 
     [SerializeField]
     private TextMeshProUGUI TextInfo;
+    [SerializeField]
+    private Slider YinSlider;
+    [SerializeField]
+    private Slider YangSlider;
+    [SerializeField]
+    private RawImage UltimateImage;
 
     public void ActivateUltimate() {
         if (YinFill != 100 || YangFill != 100) return;
@@ -24,21 +31,52 @@ public class UltimateLoad : MonoBehaviour {
         }
     }
 
+    private void Update() {
+        if (Input.GetKeyUp(KeyCode.S)) {
+            AddCharge(10, true);
+        }
+        if (Input.GetKeyUp(KeyCode.U)) {
+            AddCharge(10, false);
+        }
+    }
+
+    private void CheckFullCharge() {
+        if (YinFill >= 100 &&  YangFill >= 100) {
+            TextInfo.alpha = 1f;
+            TextInfo.text = "The ultimate is ready!";
+            StartCoroutine(FadeText());
+            
+            UltimateImage.color = new Color {
+                r = UltimateImage.color.r,
+                g = UltimateImage.color.g,
+                b = UltimateImage.color.b,
+                a = 100
+            };
+        }
+    }
+
     public void AddCharge(float amount, bool yin) {
         if (yin && YinFill < 100) {
             YinFill += amount;
+            YinSlider.value = YinFill;
+
             if (YinFill >= 100) {
                 TextInfo.alpha = 1f;
                 TextInfo.text = "Yin is charged";
                 StartCoroutine(FadeText());
+                CheckFullCharge();
             }
         }
+
         if (!yin && YangFill < 100) {
             YangFill += amount;
+            YangSlider.value = YangFill;
+
             if (YangFill >= 100) {
                 TextInfo.alpha = 1f;
                 TextInfo.text = "Yang is charged";
                 StartCoroutine(FadeText());
+                CheckFullCharge();
             }
         }
     }
